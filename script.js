43db23dfcd56bd5688ad8a579d525d70a1d1524c
HCDesktop <HaydenLCrossJunk@gmail.com>
2019-11-29 21:33:02 -0800

    created all ID variables for future JS

---
+++ script.js
@@ -0,0 +1,51 @@
+$(document).ready(function(){
+
+// Search Card Variables, Includes User Input, and the search history and it's nested lis
+var srch = document.getElementById("#input");
+var srcHx = document.getElementById("#srchx");
+var srcHxList = document.getElementsByClassName(".srchx-li");
+
+//Main Card Variables, these vars will be the current day's result of the search
+var crntCityDate = document.getElementById("#title");
+var temp = document.getElementById("#temp");
+var hum = document.getElementById("#hum");
+var wind = document.getElementById("#wind");
+var uv = document.getElementById("#uv");
+
+//5 Day Forecast Vars ---------------------------
+
+//Day One
+var dayOneDate = document.getElementById("#day-one");
+var dayOneTemp = document.getElementById("#day-one-temmp");
+var dayOneHum = document.getElementById("#day-one-hum");
+
+//Day Two
+var dayTwoDate = document.getElementById("#day-two");
+var dayTwoTemp = document.getElementById("#day-two-temmp");
+var dayTwoHum = document.getElementById("#day-two-hum");
+
+//Day Three
+var dayThreeDate = document.getElementById("#day-three");
+var dayThreeTemp = document.getElementById("#day-three-temmp");
+var dayThreeHum = document.getElementById("#day-three-hum");
+
+//Day Four
+var dayFourDate = document.getElementById("#day-four");
+var dayFourTemp = document.getElementById("#day-four-temmp");
+var dayFourHum = document.getElementById("#day-four-hum");
+
+//Day Five
+var dayFiveDate = document.getElementById("#day-five");
+var dayFiveTemp = document.getElementById("#day-five-temmp");
+var dayFiveHum = document.getElementById("#day-five-hum");
+
+//End 5 Day Vars ---------------------------------
+
+
+
+
+
+
+
+
+});
